5f0c923595375a50685d0fe5ea2fe35446b2034f
patled <github@lederer.cloud>
2024-05-09 20:49:14 +0200

    refactor: Update TaskItem component to use Task object instead of individual props

--- src/Components/TaskItem.tsx
+++ src/Components/TaskItem.tsx
@@ -1,27 +1,27 @@
 import { useContext } from "react";
 import { SpeechServiceContext } from "../SpeechServiceContext";
 import "./TaskItem.css";
+import { Task } from "../dtos/Task";
 
 type TaskItemProps = {
-  audioText: string;
-  icon?: string;
+  task: Task;
 };
 
-export function TaskItem({ audioText, icon }: TaskItemProps) {
+export function TaskItem({ task }: TaskItemProps) {
   const speechService = useContext(SpeechServiceContext);
 
   function speak() {
-    speechService.speak(audioText);
+    speechService.speak(task.audioText);
   }
 
   return (
     <div className="task" onClick={() => speak()}>
       {/* <span className="material-icons">{icon}</span> */}
-      <span className="material-icons-outlined">{icon}</span>
+      <span className="material-icons-outlined">{task.icon}</span>
       {/* <span className="material-icons-round">{icon}</span>
       <span className="material-icons-sharp">{icon}</span>
     <span className="material-icons-two-tone">{icon}</span> */}
-      <p>{audioText}</p>
+      <p>{task.audioText}</p>
     </div>
   );
 }
